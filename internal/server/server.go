00320ced8b7cb3a60686ea6060d2c35e706dd84a
jackjie20162 <610841147@qq.com>
2026-09-02 18:14:58 +0800

    feat(travel-rpc): register catalog inventory order services

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,20 @@
 package server
 
-import "github.com/zeromicro/go-zero/zrpc"
+import (
+	"google.golang.org/grpc"
 
-// Register reserves the RPC registration point for generated protobuf services.
-// Generated registration code will be wired here after the protobuf toolchain is configured.
-func Register(_ *zrpc.RpcServer) {}
+	"gitee.com/meinongyihe/travel-rpc/ent"
+	"gitee.com/meinongyihe/travel-rpc/internal/repository"
+	"gitee.com/meinongyihe/travel-rpc/internal/service"
+	"gitee.com/meinongyihe/travel-rpc/travel"
+)
+
+// Register wires all generated tourism RPC services onto the underlying gRPC server.
+func Register(grpcServer *grpc.Server, client *ent.Client) {
+	products := repository.NewProductRepository(client)
+	inventory := repository.NewInventoryRepository(client)
+	orders := repository.NewOrderRepository(client)
+	travel.RegisterCatalogServiceServer(grpcServer, service.NewCatalogService(products))
+	travel.RegisterInventoryServiceServer(grpcServer, service.NewInventoryService(inventory))
+	travel.RegisterOrderServiceServer(grpcServer, service.NewOrderService(orders, inventory))
+}
