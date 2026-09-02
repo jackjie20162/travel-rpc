24b896469639a99bfc062e291d535e44175371a6
jackjie20162 <610841147@qq.com>
2026-09-02 17:03:11 +0800

    feat: define tenant schema and indexes

--- ent/schema/tenant.go
+++ ent/schema/tenant.go
@@ -1,5 +1,24 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+)
 
 type Tenant struct { ent.Schema }
+
+func (Tenant) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("code").NotEmpty(),
+		field.String("name").NotEmpty(),
+		field.String("status").Default("ACTIVE"),
+	}
+}
+
+func (Tenant) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("code").Unique(),
+		index.Fields("status"),
+	}
+}
