d4b566eec78f37f63f9f4e7afee2b9d166ede0f5
jackjie20162 <610841147@qq.com>
2026-09-02 15:14:02 +0800

    travel-rpc: add package indexes

--- ent/schema/package.go
+++ ent/schema/package.go
@@ -3,17 +3,9 @@ package schema
 import (
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
 )
 
 type Package struct { ent.Schema }
-
-func (Package) Fields() []ent.Field {
-	return []ent.Field{
-		field.Int64("tenant_id"),
-		field.Int64("merchant_id"),
-		field.Int64("product_id"),
-		field.String("code").NotEmpty(),
-		field.String("name").NotEmpty(),
-		field.String("status").Default("ACTIVE"),
-	}
-}
+func (Package) Fields() []ent.Field { return []ent.Field{field.Int64("tenant_id"), field.Int64("merchant_id"), field.Int64("product_id"), field.String("code").NotEmpty(), field.String("name").NotEmpty(), field.String("status").Default("ACTIVE")} }
+func (Package) Indexes() []ent.Index { return []ent.Index{index.Fields("tenant_id", "product_id", "code").Unique(), index.Fields("tenant_id", "product_id", "status")} }
