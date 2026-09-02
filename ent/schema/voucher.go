e751b97511bd93868d08d1f9c875422c99f9428f
jackjie20162 <610841147@qq.com>
2026-09-02 15:14:18 +0800

    travel-rpc: add voucher indexes

--- ent/schema/voucher.go
+++ ent/schema/voucher.go
@@ -3,16 +3,9 @@ package schema
 import (
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
 )
 
 type Voucher struct { ent.Schema }
-
-func (Voucher) Fields() []ent.Field {
-	return []ent.Field{
-		field.Int64("tenant_id"),
-		field.Int64("order_id"),
-		field.String("voucher_no").NotEmpty(),
-		field.String("status").Default("ISSUED"),
-		field.String("redeemed_at").Optional(),
-	}
-}
+func (Voucher) Fields() []ent.Field { return []ent.Field{field.Int64("tenant_id"), field.Int64("order_id"), field.String("voucher_no").NotEmpty(), field.String("status").Default("ISSUED"), field.String("redeemed_at").Optional()} }
+func (Voucher) Indexes() []ent.Index { return []ent.Index{index.Fields("voucher_no").Unique(), index.Fields("tenant_id", "order_id"), index.Fields("tenant_id", "status")} }
