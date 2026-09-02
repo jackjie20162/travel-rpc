ed041f8e126f9f760a9a6aaf413a6f14846c29be
jackjie20162 <610841147@qq.com>
2026-09-02 18:15:10 +0800

    fix(travel-rpc): wire generated gRPC services at startup

--- travel.go
+++ travel.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 
 	"gitee.com/meinongyihe/travel-rpc/internal/config"
 	"gitee.com/meinongyihe/travel-rpc/internal/db"
 	"gitee.com/meinongyihe/travel-rpc/internal/server"
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
 )
 
 var configFile = flag.String("f", "etc/travel-rpc.yaml", "config file")
@@ -18,13 +20,13 @@ func main() {
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 
-	client, err := db.NewClient(context.Background(), "mysql", c.DatabaseConf.DataSource())
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=true&loc=UTC", c.DatabaseConf.Username, c.DatabaseConf.Password, c.DatabaseConf.Host, c.DatabaseConf.Port, c.DatabaseConf.Dbname)
+	client, err := db.NewClient(context.Background(), "mysql", dsn)
 	if err != nil { panic(err) }
 	defer client.Close()
 
-	rpcServer := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *zrpc.RpcServer) {
-		// zRPC invokes this callback with the concrete gRPC registrar in the generated server template.
-		_ = grpcServer
+	rpcServer := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		server.Register(grpcServer, client)
 	})
 	defer rpcServer.Stop()
 	rpcServer.Start()
